4991071ec2a3dd1cfaf604547be86626639d01a0
gustavomachado19 <gustavo2002machado@hotmail.com>
2025-05-15 13:08:49 -0300

    API para estudos de cloud

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, HTTPException
+from models import Tarefa
+
+app = FastAPI()
+
+tarefas = []
+
+@app.get("/tarefas")
+def listar_tarefas():
+    return tarefas
+
+@app.post("/tarefas")
+def criar_tarefa(tarefa: Tarefa):
+    tarefas.append(tarefa)
+    return {"mensagem": "Tarefa criada com sucesso"}
+
+@app.delete("/tarefas/{id}")
+def deletar_tarefa(id: int):
+    for tarefa in tarefas:
+        if tarefa.id == id:
+            tarefas.remove(tarefa)
+            return {"mensagem": "Tarefa removida"}
+    raise HTTPException(status_code=404, detail="Tarefa não encontrada")
---
+++ models.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+
+class Tarefa(BaseModel):
+    id: int
+    titulo: str
+    concluida: bool = False
