d2079ac16e362137616e05d68bd16a0a8d655be2
Gustavo Pedroso <gustavo2002machado@hotmail.com>
2025-05-18 11:55:52 -0300

    Adicionando cache redis na api

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 from fastapi import FastAPI, HTTPException
 from models import Tarefa
+import redis
+import json
+import os
 
 app = FastAPI()
 
-tarefas = []
+# Substitua pelo endpoint do seu Redis
+redis_host = "seu-endpoint-do-redis.amazonaws.com"
+redis_port = 6379  # Porta padrão
+r = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
+
 @app.get("/")
 def read_root():
-    return {"message": "API para estudos cloud rodando com sucesso!"}
+    return {"message": "API com Redis funcionando!"}
 
 @app.get("/tarefas")
 def listar_tarefas():
-    return tarefas
+    tarefas = r.lrange("tarefas", 0, -1)
+    return [json.loads(tarefa) for tarefa in tarefas]
 
 @app.post("/tarefas")
 def criar_tarefa(tarefa: Tarefa):
-    tarefas.append(tarefa)
+    r.rpush("tarefas", json.dumps(tarefa.dict()))
     return {"mensagem": "Tarefa criada com sucesso"}
 
 @app.delete("/tarefas/{id}")
 def deletar_tarefa(id: int):
+    tarefas = r.lrange("tarefas", 0, -1)
     for tarefa in tarefas:
-        if tarefa.id == id:
-            tarefas.remove(tarefa)
+        tarefa_json = json.loads(tarefa)
+        if tarefa_json["id"] == id:
+            r.lrem("tarefas", 1, tarefa)
             return {"mensagem": "Tarefa removida"}
     raise HTTPException(status_code=404, detail="Tarefa não encontrada")
